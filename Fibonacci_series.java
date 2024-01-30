e4a7375d7e6b41e287572bc18d55f4dc216d8500
Tushar kumar <tusharnwd2003@gmail.com>
2024-01-30 18:28:33 +0530

    fibonacci_series

---
+++ Fibonacci_series.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci_series {
+  public static void main(String[] args) {
+    System.out.print("Enter the number");
+    try ( Scanner sc = new Scanner(System.in)){
+        int n = sc.nextInt();
+      System.out.println( n + "th Fibonacci Number: " + fib(n));
+    }
+  }
+
+  static int fib(int number) {
+
+    if (number <= 1) {
+      return number;
+    }
+    return fib(number - 1) + fib(number - 2);
+  }
+}
